119860eabd16605b314fb764932eb7f3552ebfa0
PaulJungnickel <jungnikp@gmail.com>
2025-03-30 12:54:02 +0200

    graded3 wip

---
+++ week8/Data_Code_Sheet_14/Animation_Polymer.py
@@ -0,0 +1,37 @@
+import numpy as np
+from pylab import *
+from matplotlib import pyplot as plt
+from matplotlib import animation
+
+trajectory = loadtxt('rousecoords2d_sample.txt')
+
+# First set up the figure, the axis, and the plot element we want to animate
+fig = plt.figure()
+ax = plt.axes(xlim=(0, 20), ylim=(0,15))
+line, = ax.plot([], [], "-o", color="b", markevery=1)
+
+# initialization function: plot the background of each frame
+def init():
+    line.set_data([], [])
+    return line, 
+
+# animation function.  This is called sequentially
+def animate(i):
+    x = (trajectory[i*6,:]+2.5)
+    y = (trajectory[i*6+1,:]+7.5)
+    line.set_data(x, y)
+    return line,
+
+# call the animator.  blit=True means only re-draw the parts that have changed.
+anim = animation.FuncAnimation(fig, animate, init_func=init,
+                               frames=1500, interval=10, blit=True)
+
+# save the animation as an mp4.  This requires ffmpeg or mencoder to be
+# installed.  The extra_args ensure that the x264 codec is used, so that
+# the video can be embedded in html5.  You may need to adjust this for
+# your system: for more information, see
+# http://matplotlib.sourceforge.net/api/animation_api.html
+
+#anim.save('polymer.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+
+plt.show()
---
+++ week8/Data_Code_Sheet_14/README.txt
@@ -0,0 +1,5 @@
+This directory contains example data in case you didn't manage to simulate the Rouse model by yourself.
+
+rousecoords2d_sample.txt has the coordinates for a 2D simulation, which can be animated using Animation_Polymer.py.
+
+rousedat_end2end__runs_[]_N_[].txt files contain two rows of data. Row 1 gives the time, and row 2 gives the mean end-to-end vector deviation at the time. These data can be used for the plots in exercises 1d and 1e.
